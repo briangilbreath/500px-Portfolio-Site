82021d8ca79e672b343e259027d2a4976d286a03
briangilbreath <bgilbreath87@gmail.com>
2016-06-12 18:50:03 -0400

    test

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
+            'driver' => 'mysql', 
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
