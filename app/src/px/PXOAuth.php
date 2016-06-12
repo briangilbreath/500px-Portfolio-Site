1652180ff575e194d02fd4c308cdad4256a01050
briangilbreath <bgilbreath87@gmail.com>
2016-06-12 18:42:11 -0400

    test

--- app/src/px/PXOAuth.php
+++ app/src/px/PXOAuth.php
@@ -14,7 +14,7 @@ class PXOAuth{
 
         $this->oauth = new Oauth(
             getenv('500PX_KEY'),
-            getenv('500PX_SECRET'),
+            getenv('500PX_SECRET'), 
             'https://api.500px.com/v1/'
         );
 
