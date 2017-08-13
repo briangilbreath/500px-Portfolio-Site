09bace5a7cd601c19b7c0718deb76724f79f81ce
briangilbreath <bgilbreath87@gmail.com>
2017-08-13 14:32:54 -0400

    added roku projects

---
+++ resources/views/portfolio/bbcamerica-roku.blade.php
@@ -0,0 +1,11 @@
+<div class="slider project-slider ">
+  <div><h3><a href="/img/technology/bbcamerica_roku/bbcamerica_roku_home.jpg" class="swipebox"><img src="/img/technology/bbcamerica_roku/bbcamerica_roku_home.jpg" alt="BBC America Roku" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_roku/bbcamerica_roku_show.jpg" class="swipebox"><img src="/img/technology/bbcamerica_roku/bbcamerica_roku_show.jpg" alt="BBC America Roku" /></a></h3></div>
+</div>
+
+<div class="desc">
+  <p>Tech lead for BBC America's Roku app. Designed API output and architecture to power module based app. Pioneered
+    API powered web prototype system that drove Roku design and layout before application coding.
+    Worked with project and product teams to create project roadmap, sharpen product requirements, and meet
+    tight deadline. Worked with development teams to structure app and fix bugs.</p>
+</div>
--- resources/views/portfolio/bbcamerica.blade.php
+++ resources/views/portfolio/bbcamerica.blade.php
@@ -1,7 +1,13 @@
 <div class="slider project-slider ">
   <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_homepage.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_homepage.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_nav_expanded.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_nav_expanded.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_show_page.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_show_page.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_full_episodes_page.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_full_episodes_page.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_schedule.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_schedule.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_blog_page.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_blog_page.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_mobile_homepage.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_mobile_homepage.jpg" alt="bbcamerica.com" /></a></h3></div>
+  <div><h3><a href="/img/technology/bbcamerica_com/bbcamerica_com_mobile_homepage_expanded.jpg" class="swipebox"><img src="/img/technology/bbcamerica_com/bbcamerica_com_mobile_homepage_expanded.jpg" alt="bbcamerica.com" /></a></h3></div> 
 </div>
-
 
 <div class="desc">
   <p>Lead front end developer for bbcamerica.com, and API's powering Mobile and OTT apps. Using HTML/JS/SASS, developed design and layout for entire site.
